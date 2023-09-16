5c32d93232c0532414a417ca7ffafe366e095fb3
scridonlucas <lukas_scridon@yahoo.com>
2023-09-16 22:11:51 +0300

    added first post requests, application working as expected

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,16 +1,18 @@
 import { Router } from 'express';
 import toNewUserEntry from '../utils/validators/userValidator/userValidator';
 import { NewUser } from '../../types';
-import { addUser } from '../services/users';
+import { addUser, getUsers } from '../services/users';
 const usersRouter = Router();
 
 usersRouter.get('/', (_req, res) => {
-  res.send('pong');
+  res.send(getUsers());
 });
 
 usersRouter.post('/', (req, res) => {
   try {
     const newUserEntry: NewUser = toNewUserEntry(req.body);
+    const addedUser = addUser(newUserEntry);
+    res.json(addedUser);
   } catch (error: unknown) {
     if (error instanceof Error) {
       res.status(400).send(error.message);
--- src/services/users.ts
+++ src/services/users.ts
@@ -1,6 +1,9 @@
 import { NewUser, User } from '../../types';
 const users: User[] = [];
 
+export const getUsers = (): User[] => {
+  return users;
+};
 export const addUser = (newUser: NewUser): User => {
   const user: User = {
     id: newUser.username,
