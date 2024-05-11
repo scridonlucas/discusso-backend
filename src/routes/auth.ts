9f920f88e53059b9d239c895f91d71b07a8ad3b5
scridonlucas <lukas_scridon@yahoo.com>
2024-05-11 18:11:15 +0300

    auth route implemented

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,7 +1,14 @@
 import 'express-async-errors';
 import { Router } from 'express';
 import { RequestHandler } from 'express';
+import middleware from '../utils/middleware';
+import { CustomRequest } from '../types/types';
 
 const authRouter = Router();
 
-authRouter.get('/', (async (_req, _res) => {}) as RequestHandler);
+authRouter.get('/', middleware.jwtVerify, ((req: CustomRequest, res) => {
+  const decodedToken = req.user;
+  res.json({ decodedToken });
+}) as RequestHandler);
+
+export default authRouter;
