7b14972be3a1fe9b6f821aa7de7d657ecbfafc2e
scridonlucas <lukas_scridon@yahoo.com>
2024-06-15 22:59:48 +0300

    implementing prisma instead of drizzle

--- src/utils/db.ts
+++
@@ -1,9 +0,0 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
-import config from './config';
-
-const connectionString = config.POSTGRES_DB_URL;
-
-const db = drizzle(postgres(connectionString));
-
-export default db;
