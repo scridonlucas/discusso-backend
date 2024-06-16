6ef65b0f7dd3d03fa4ca3b71b7bf00c0d2daa69a
scridonlucas <lukas_scridon@yahoo.com>
2024-06-16 20:01:54 +0300

    completely got rid off sequelize and drizzle, replaced them with prisma orm

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
 import 'express-async-errors';
 import app from './app';
 import config from './utils/config';
-import database from './utils/database';
 
-const { dbConnect } = database;
 const { PORT } = config;
 
-const start = async () => {
-  await dbConnect();
+const start = () => {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
