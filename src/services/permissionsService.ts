26b4fd8a86ef73e1d7b36f6c30f6578884d1883a
scridonlucas <lukas_scridon@yahoo.com>
2024-08-17 18:35:22 +0300

    check permissions in db done

--- src/services/permissionsService.ts
+++ src/services/permissionsService.ts
@@ -1,1 +1,33 @@
 import prisma from '../utils/prismaClient';
+
+const hasPermission = async (
+  userId: number,
+  permissionName: string
+): Promise<boolean> | null => {
+  const user = await prisma.user.findUnique({
+    where: { id: userId },
+    include: {
+      role: {
+        include: {
+          permissions: {
+            include: {
+              permission: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  if (!user) {
+    return null;
+  }
+
+  return user.role.permissions.some(
+    (rp) => rp.permission.permissionName === permissionName
+  );
+};
+
+export default {
+  hasPermission,
+};
