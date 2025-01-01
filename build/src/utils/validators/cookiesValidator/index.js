4b0d3c4541e075f5e3bab65049f6ba3243139cc8
scridonlucas <lukas_scridon@yahoo.com>
2025-01-01 17:13:46 +0200

    fixed issues and removed build version from git

--- build/src/utils/validators/cookiesValidator/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const cookiesValidator_1 = __importDefault(require("./cookiesValidator"));
-exports.default = { toNewCookie: cookiesValidator_1.default };
--- build/src/utils/validators/loginValidator/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const loginValidator_1 = __importDefault(require("./loginValidator"));
-exports.default = { toNewLoginEntry: loginValidator_1.default };
